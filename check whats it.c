4d786ad007853f8e6a12e48e1cf5fe73a696d777
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-04-05 21:47:31 +0530

    Check it

---
+++ check whats it.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	//program to check given character is an alphabet, digit or special character.
+	printf(">> A Program by Vedansh <<\n");
+	char main;
+	printf("Enter A Character: ");
+	scanf("%c", &main);
+	if((main >='a' && main <='z') || (main >='A' && main <='Z'))
+	   printf("The Given Character is an Alphabet  = %c", main);
+	else if(main >= '0' && main <= '9')
+	   printf("%c is an Number", main);
+	else
+	   printf("%c | It's an Special Character", main);
+	return 0;
+}
