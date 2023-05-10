a752401d231d3e161218b64badbc8dfa1f750fc0
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-05-11 00:03:32 +0530

    Programs

---
+++ recursion.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+void Show(int n){ 
+	if(n>0){
+		printf("\t %d ",n);
+		Show(n-1);
+	}
+}
+int main(){
+	//program, i.e. calling the function
+	int x;
+	printf("Enter Your Lucky Number : ");
+	scanf("%d", &x);
+	Show(x);
+}
