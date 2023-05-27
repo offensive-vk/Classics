56cc2e01d137fb71e835ef4b2fc8bbf5d583d418
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-05-27 16:31:51 +0530

    Programs

---
+++ union.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+/*
+	same like structure, a union is a data type in C which can store different types of data.
+	It is also a collection of members.
+	A union stores data in the same memory location.
+	Only one member can contain a value at a time.
+*/
+
+union list{
+	char name[50];
+	int pin;
+	float x;
+};
+
+int main(){
+	int x,y;
+	union list data;
+	//strcpy(data.name,'Madhav');
+	data.pin = 9011;
+	printf("\n %d \n", data.pin);
+	data.x = 90.22;
+
+	//printf("\n\t %s\n", data.name);
+	printf("\n %f \n", data.x);
+	return 0;
+}
