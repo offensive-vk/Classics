a31dbadd9e28a3c8bdace0bbab16c3e718022ead
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-05-27 16:32:06 +0530

    Programs

---
+++ vowel in string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+// wap is total number of vowel present in the given string.
+int main(){
+	int count=0, i,j, length;
+	char str[100];
+	printf("Enter String : ");
+	gets(str);
+	length = strlen(str);
+	for(i=0; i<length; i++)
+	{
+		printf("%s", i);
+	}
+    
+}
