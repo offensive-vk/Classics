890caf98363323d63a3b0996de8a9bfd2a79c7b4
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-08 09:54:46 +0530

    cpp

---
+++ C++/access.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+class Access {
+public:
+    int publicVar;    // Public member variable
+
+    void publicMethod() {
+        std::cout << "Public method called." << std::endl;
+    }
+
+protected:
+    int protectedVar; // Protected member variable
+
+    void protectedMethod() {
+        std::cout << "Protected method called." << std::endl;
+    }
+
+private:
+    int privateVar;   // Private member variable
+
+    void privateMethod() {
+        std::cout << "Private method called." << std::endl;
+    }
+};
+
+int main() {
+    Access obj;
+
+    // Accessing public members
+    obj.publicVar = 42;
+    obj.publicMethod();
+
+    // Cannot access protected and private members directly from the main function
+    // obj.protectedVar = 30;
+    // obj.protectedMethod();  
+
+    // obj.privateVar = 20; 
+    // obj.privateMethod();
+
+    return 0;
+}
