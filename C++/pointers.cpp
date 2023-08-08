3b3919c854ff22888c52ed242b16b1ffbd7704eb
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-08 09:28:18 +0530

    cpp

--- C++/pointers.cpp
+++ C++/pointers.cpp
@@ -1,13 +1,20 @@
-#include <iostream>
+#include<iostream>
+#include<conio.h>
 
 int main() {
     int num = 10;
     int* ptr = &num;
+    int* ptx = new int;
 
     std::cout << "Value of num: " << num << std::endl;
     std::cout << "Address of num: " << &num << std::endl;
     std::cout << "Value of ptr: " << ptr << std::endl;
     std::cout << "Value pointed by ptr: " << *ptr << std::endl;
 
+    *ptx = 999;
+    std::cout << "Value hold by ptx pointer is " << *ptx << std::endl;
+    delete ptr;
+    
     return 0;
 }
+
