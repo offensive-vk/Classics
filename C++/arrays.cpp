559dfa14580eb221b02c80f86564099755f81b5d
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-08 09:23:45 +0530

    cpp

---
+++ C++/arrays.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<conio.h>
+const int SIZE = 5;
+
+int main() {
+    // datatype arrayName [size];
+    int numbers[SIZE]; 
+
+    numbers[0] = 10;
+    numbers[1] = 20;
+    numbers[2] = 30;
+    numbers[3] = 40;
+    numbers[4] = 50;
+
+    std::cout << "Array Elements Are: ";
+    for (int i = 0; i < SIZE; i++) {
+        std::cout << numbers[i] << " ";
+    }
+    std::cout << std::endl;
+
+    numbers[2] = 35;
+    numbers[4] += 10;
+
+        std::cout << "Modified array elements: ";
+    for (int i = 0; i < SIZE; i++) {
+        std::cout << numbers[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
