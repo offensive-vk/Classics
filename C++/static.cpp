711a10ae2bc3c14b9f96c295129c0da0d96cbac0
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-09 19:03:03 +0530

    c++

---
+++ C++/static.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <conio.h>
+
+class MyClass {
+public:
+    static int staticVar;
+
+    static void staticMethod() {
+        std::cout << "Static method called." << std::endl;
+    }
+};
+
+int MyClass::staticVar = 0;
+
+int main() {
+
+    MyClass::staticVar = 42;
+    std::cout << "Static variable value: " << MyClass::staticVar << std::endl;
+
+    MyClass::staticMethod();
+
+    return 0;
+}
