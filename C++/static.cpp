8c558f434826efaac454332e5539fa3f6da15ef3
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-09 20:10:35 +0530

    c++

--- C++/static.cpp
+++ C++/static.cpp
@@ -9,15 +9,13 @@ public:
         std::cout << "Static method called." << std::endl;
     }
 };
-
 int MyClass::staticVar = 0;
 
 int main() {
 
     MyClass::staticVar = 42;
     std::cout << "Static variable value: " << MyClass::staticVar << std::endl;
-
     MyClass::staticMethod();
-
     return 0;
+
 }
