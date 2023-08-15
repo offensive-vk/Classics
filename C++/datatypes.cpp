633fc61bf8620ec8626691ca661af4a206b99fe5
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-15 12:08:00 +0530

    c++

--- C++/datatypes.cpp
+++ C++/datatypes.cpp
@@ -4,14 +4,14 @@
 int main(int argc, char const *argv[])
 {
     // Pre Defined Data Types in C++
-    int x;
-    float y;
-    char z;
-    double a;
-    long b;
-    short s;
-    bool c;
-    std::string name;
+    int x = 10;
+    float y = 10.22;
+    char z = 'A';
+    double a = 22.102;
+    long b = 23453701543;
+    short s = 345331534.74;
+    bool c = false;
+    std::string name = "C++";
     std::cout << "All Data Types of C are Supported in C++ " << std::endl;
 
     return 0;
