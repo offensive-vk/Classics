c5e1a12a324202de9cf8cc451550596e09f84b86
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-15 13:37:11 +0530

    c++

--- C++/hello.cpp
+++ C++/hello.cpp
@@ -4,3 +4,17 @@ int main()
     std::cout << "Hello C++ ! Whats Up ." << std::endl;
     return 0;
 }
+/*  Escape Sequences
+\b	Backspace
+\f	Form feed
+\n	Newline
+\r	Return
+\t	Horizontal tab
+\v	Vertical tab
+\\	Backslash
+\'	Single quotation mark
+\"	Double quotation mark
+\?	Question mark
+\0	Null Character
+
+*/
