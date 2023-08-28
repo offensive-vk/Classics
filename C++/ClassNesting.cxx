2475b6001c50de2c0f7d4f31786e4d541c891214
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-28 09:16:43 +0530

    cpp

--- C++/ClassNesting.cxx
+++ C++/ClassNesting.cxx
@@ -1,10 +1,27 @@
 #include<iostream>
 #include<conio.h>
 #include<windows.h>
+static const char limit = 'A';
 
-int main(int argc, char const * argv[])
+class Parent{
+    public:
+        int id;
+        int reg;
+    Parent(int id, int reg) : id(id), reg(reg) {
+        std::cout << "\t :: Constructor Has been Called :: \n" << std::endl;
+        // this->id = id;
+    }
+    void show(){
+        std::cout << this->id << std::endl;
+        std::cout << this->reg << std::endl;
+
+    }
+};
+
+int main()
 {
     std::cout << "\t :: Class Nesting:: \n" << std::endl;
-    
+    Parent p(9001, 454201);
+    p.show();
     return 0;
 }
