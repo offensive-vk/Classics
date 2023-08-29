bf8bd363677a09a243b3c012ebdb103346007b27
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-29 19:18:12 +0530

    cpp

---
+++ C++/Perfect.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+
+    int i = 1, u = 1, sum = 0;
+    while (i <= 500)
+    { // start of first loop.
+
+        while (u <= 500)
+        { // start of second loop.
+            if (u < i)
+            {
+                if (i % u == 0)
+                    sum = sum + u;
+            }
+
+            u++;
+        } // End of second loop
+
+        if (sum == i)
+        {
+            cout << i << " is a perfect number." << "\n";
+        }
+
+        i++;
+        u = 1;
+        sum = 0;
+    } // End of First loop
+    
+    getch();
+}
