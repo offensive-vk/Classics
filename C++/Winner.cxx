a3219fafdbdccfd627b1f0865ab6af9d4166c1dd
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-31 16:51:19 +0530

    cpp

--- C++/Winner.cxx
+++ C++/Winner.cxx
@@ -1,16 +1,26 @@
 #include <iostream>
 
-int calculate_sum(int n) {
-    int total = 0;
-    for (int i = 1; i <= n; ++i) {
-        total += i;
-    }
-    return total;
-}
+// int calculate_sum(int n) {
+//     int total = 0;
+//     for (int i = 1; i <= n; ++i) {
+//         total += i;
+//     }
+//     return total;
+// }
 
-int main() {
-    int N = 10000000;
-    int result = calculate_sum(N);
-    std::cout << result << std::endl;
+// int main() {
+//     int N = 10000000;
+//     int result = calculate_sum(N);
+//     std::cout << result << std::endl;
+//     return 0;
+// }
+
+int main(){
+    int i = 0;
+    while (i< 1000000000)
+    {
+        i++;
+    }
+    std::cout << i << std::endl;
     return 0;
 }
