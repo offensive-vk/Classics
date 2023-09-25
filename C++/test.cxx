6fc0d169e48296956a8b9fc853e3ab2ee287a573
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-25 09:47:08 +0530

    new

---
+++ C++/test.cxx
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<conio.h>
+
+class Source{
+private:
+    unsigned int id;
+    unsigned int age; 
+    unsigned int *data[5] = {};
+public:
+    std::string bases = " ";
+    Source(unsigned int id, unsigned int age, std::string bases)
+    {
+        this->id = id;
+        this->age = age;
+    }
+    void display(){
+        std::cout << "Your Id : " << this->id << std::endl;
+        std::cout << "Your Age : " << this->age << std::endl;
+        std::cout << " Thank you . " << std::endl;
+    }
+    ~Source(){ std::cout << "Goodbye Source class members. " << std::endl; }
+};
+
+int main(int argc, char const *argv[])
+{
+    Source *X;
+    
+    return 0;
+}
