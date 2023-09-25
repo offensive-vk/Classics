0c25812c75a1121efd12d0a0be242d72fd73bdfe
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-25 16:01:12 +0530

    c++

--- C++/SmartPtr.cpp
+++ C++/SmartPtr.cpp
@@ -16,3 +16,18 @@ public:
     }
 };
 
+int main() {
+    unique_ptr<MyClass> ptr1(new MyClass);
+    ptr1->hello();
+
+    shared_ptr<MyClass> ptr2 = make_shared<MyClass>();
+    ptr2->hello();
+
+    weak_ptr<MyClass> ptr3 = ptr2;
+    cout << "ptr3.use_count() = " << ptr3.use_count() << endl;
+
+    shared_ptr<MyClass> ptr4 = ptr3.lock();
+    ptr4->hello();
+
+    return 0;
+}
