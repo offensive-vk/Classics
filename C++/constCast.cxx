265d1395884a3d7e7ca84100c6da461f2aa6af45
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-26 10:04:06 +0530

    c++

--- C++/constCast.cxx
+++ C++/constCast.cxx
@@ -1,5 +1,4 @@
 #include <iostream>
-
 void modifyVariable(int* ptr) {
     *ptr = 42;
 }
@@ -14,3 +13,6 @@ int main() {
 
     return 0;
 }
+/* 
+const_cast is a type of casting in C++ that allows you to remove or add constness to a variable. In other words, it enables you to modify a const or volatile object, or change a pointer or reference to a const or volatile type. This is useful in certain scenarios when you need to pass a const variable as an argument or when a function parameter requires a non-const type, but you want to make sure the variable remains constant throughout the code.
+*/
