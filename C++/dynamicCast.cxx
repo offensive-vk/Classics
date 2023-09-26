975049086f03d122bb31776ff61789d562757d50
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-26 10:04:30 +0530

    c++

--- C++/dynamicCast.cxx
+++ C++/dynamicCast.cxx
@@ -27,3 +27,7 @@ int main() {
     delete basePtr;
     return 0;
 }
+/* 
+dynamic_cast is a type of casting operator in C++ that is used specifically for polymorphism. It safely converts pointers and references of a base class to its derived class and checks the validity of the conversion during runtime. If the conversion is not valid (i.e., the object is not of the target type), it returns a null pointer instead of producing undefined behavior. Therefore, dynamic_cast can prevent potential crashes and errors when using polymorphism.
+
+*/
