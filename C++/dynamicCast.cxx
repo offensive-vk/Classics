9578d90b1f221bc0aae1add133ad9da5ae81c3e1
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-26 10:02:05 +0530

    c++

---
+++ C++/dynamicCast.cxx
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class BaseClass {
+   public:
+    virtual void display() {
+        std::cout << "BaseClass" << std::endl;
+    }
+};
+
+class DerivedClass : public BaseClass {
+   public:
+    void display() {
+        std::cout << "DerivedClass" << std::endl;
+    }
+};
