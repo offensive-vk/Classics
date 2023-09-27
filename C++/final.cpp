e1b6e103e8e7d22f63db6d0ede175630dfb54ab0
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-27 09:49:45 +0530

    c++

--- C++/final.cpp
+++ C++/final.cpp
@@ -12,4 +12,17 @@ public:
         std::cout << "Base::nonVirtualFunction()" << std::endl;
     }
 };
+// Derived class attempting to override a 'final' function
+class Derived1 : public Base {
+public:
+    // Attempting to override 'foo' will result in a compilation error.
+    // void foo() override { /* ... */ } // Error: 'foo' in 'Derived1' cannot override final 'foo' in 'Base'
+};
 
+// Another derived class with a different function
+class Derived2 : public Base {
+public:
+    void bar() {
+        std::cout << "Derived2::bar()" << std::endl;
+    }
+};
