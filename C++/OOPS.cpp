382b27d909e398fd433993938c34e59d11fd484a
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-27 09:35:00 +0530

    c++

---
+++ C++/OOPS.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cmath>
+
+// Define an abstract class 'Shape' with common properties and a virtual method
+class Shape {
+protected:
+    std::string color;
+
+public:
+    Shape(const std::string& color) : color(color) {}
+
+    // Virtual method for calculating area
+    virtual double calculateArea() const = 0;
+
+    // Getters and setters
+    std::string getColor() const {
+        return color;
+    }
+
+    void setColor(const std::string& newColor) {
+        color = newColor;
+    }
+};
+
+// Define a concrete class 'Circle' that inherits from 'Shape'
+class Circle : public Shape {
+private:
+    double radius;
+
+public:
+    Circle(const std::string& color, double radius) : Shape(color), radius(radius) {}
+
+    double calculateArea() const override {
+        return M_PI * radius * radius;
+    }
+
+    // Additional method specific to circles
+    double getRadius() const {
+        return radius;
+    }
+};
+
+// Define another concrete class 'Rectangle' that inherits from 'Shape'
+class Rectangle : public Shape {
+private:
+    double width;
+    double height;
+
+public:
+    Rectangle(const std::string& color, double width, double height) : Shape(color), width(width), height(height) {}
+
+    double calculateArea() const override {
+        return width * height;
+    }
+
+    // Additional methods specific to rectangles
+    double getWidth() const {
+        return width;
+    }
+
+    double getHeight() const {
+        return height;
+    }
+};
+
