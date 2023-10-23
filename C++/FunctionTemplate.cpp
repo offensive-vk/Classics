6301cfb23f4b853c43a5daa12ad44f3652c94b1c
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-23 18:44:13 +0530

    Final

--- C++/FunctionTemplate.cpp
+++ C++/FunctionTemplate.cpp
@@ -12,13 +12,25 @@ template <typename Type1, typename Type2> void Global(Type1 t, Type2 v) {
 
     std::cout << "It Works !" << std::endl;
 }
-
+template <typename TData> TData Attack(TData data){
+    
+    std::cout << "\n\n" << std::endl;
+    
+    int count = 0;
+    while(count <= 10){
+        std::cout << data << std::endl;
+        count++;
+    }
+    return data;
+}
 int main(int argc, char const *argv[])
 {
     std::cout << "\n === Begin Program === \n" << std::endl;
 
     Global<int, int>(101, 102);
-    
+    Attack<double>(555.555);
     std::cout << "\n === End Program === \n" << std::endl;
     return 0;
 }
+
+
