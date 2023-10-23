065243aa79b8f67ab668a3cbe0453584cb5200a2
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-23 14:34:39 +0530

    C++

--- C++/templates.cpp
+++ C++/templates.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-
+/**
+ * @authors Vedansh
+*/
 // Generic Pair class using templates
-template <typename T1, typename T2>
+template <typename Type1, typename Type2>
 class Pair {
 public:
     Pair(T1 first, T2 second) : first_(first), second_(second) {}
 
-    T1 getFirst() const {
+    Type1 getFirst() const {
         return first_;
     }
 
-    T2 getSecond() const {
+    Type2 getSecond() const {
         return second_;
     }
 
@@ -19,8 +21,8 @@ public:
     }
 
 private:
-    T1 first_;
-    T2 second_;
+    Type1 first_;
+    Type2 second_;
 };
 int main() {
     // Create pairs of different data types
