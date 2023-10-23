7ca7e5e71fe84adb7e4b47a33ab5a6b14f86ec46
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-23 16:41:20 +0530

    ~

--- C++/ClassTemplate.cpp
+++ C++/ClassTemplate.cpp
@@ -3,16 +3,21 @@
 
 template <class T> class Normal{
     public: 
-    Normal (const &T){
-        this->val = 'Constructor';
+        std::string val;
+    Normal (){
+        std::cout << "Default constructor called" << std::endl;
+    }
+    Normal (const T normal){
+        this->val = "Constructor";
     } 
 
-    T display() final{
+    T display() const{
         std::cout << "Value: " << this->val << std::endl;
     }
 };
 int main(int argc, char const *argv[])
 {
-    
+    Normal<int> N;
+
     return 0;
 }
