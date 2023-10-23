c81c6af3427619290c763cb97381d31394ec00ad
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-23 18:24:33 +0530

    Const

--- C++/FunctionTemplate.cpp
+++ C++/FunctionTemplate.cpp
@@ -1,12 +1,24 @@
 #include<iostream>
 #include<conio.h>
 
-int main(int argc, char const *argv[])
-{
+// Global Test Function.
+template <typename Type1, typename Type2> void Global(Type1 t, Type2 v) {
     
-    return 0;
+    auto temp1 = t;
+    auto temp2 = v;
+
+    std::cout << t << std::endl;
+    std::cout << v << std::endl;
+
+    std::cout << "It Works !" << std::endl;
 }
 
-int main(){
+int main(int argc, char const *argv[])
+{
+    std::cout << "\n === Begin Program === \n" << std::endl;
+
+    Global<int, int>(101, 102);
     
+    std::cout << "\n === End Program === \n" << std::endl;
+    return 0;
 }
