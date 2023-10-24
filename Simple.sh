e2c419225134e5b798ae0092087c3cccc2582b76
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-24 10:53:18 +0530

    ~

---
+++ Simple.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This is a large but simple Bash script
+
+# Declare some variables
+name="Bard"
+age=2
+favorite_color="blue"
+
+# Print a greeting to the user
+echo "Hello, $name!"
+
+# Print the user's age
+echo "You are $age years old."
+
+# Print the user's favorite color
+echo "Your favorite color is $favorite_color."
+
+# Loop through a list of numbers
+for i in {1..100}; do
+  echo "$i"
+done
+
+# Print a farewell message to the user
+echo "Goodbye, $name!"
