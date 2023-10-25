c9dbbe38551c48c5d72cea9ba41a7023a26f51fc
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-25 16:58:26 +0530

    ~

--- C++/ClassTemplate.cpp
+++ C++/ClassTemplate.cpp
@@ -1,48 +1,57 @@
-#include<iostream>
-#include<conio.h>
+#include <iostream>
+#include <windows.h>
 
-template <class T> class Normal{
-    public: 
-        T val;
-    Normal (){
+template <class T> class Normal {
+public:
+    T val;
+    Normal() {
         std::cout << "Default constructor called" << std::endl;
     }
-    Normal (const T normal){
+    Normal(const T normal) {
         this->val = normal;
-    } 
-    ~Normal(){
-        std::cout << "Special Class Object Destroyed." << std::endl;
     }
-    T display() const{
+    ~Normal() {
+        std::cout << "Normal Class Object Destroyed." << std::endl;
+    }
+    T display() const {
         std::cout << "Value: " << this->val << std::endl;
     }
 };
 
-template <class X> class Special: public Normal {
-    static int count = 0;
-    public:
-    Special(){
+template <class X> class Special : public Normal<bool> {
+    static int count;
+public:
+    Special() {
         count++;
         std::cout << "Special Class Default Constructor." << std::endl;
     }
-    ~Special(){
+    ~Special() {
         count--;
         std::cout << "Special Class Object Destroyed." << std::endl;
     }
-    static X ShowCount(){
-        std::cout << "Total Objects Created : " << Special->count << std::endl;
+    static X ShowCount() {
+        std::cout << "Total Objects Created : " << count << std::endl;
         return count;
     }
 };
-int main(int argc, char const *argv[])
-{
+
+// Initialize the static member count
+template <class X> int Special<X>::count = 0;
+
+int main(int argc, char const *argv[]) {
     /* == Class Normal ==  */
     Normal<int> N;
     Normal<double> D(55.66);
     N.display();
     D.display();
-
+    
     /* == Class Special ==  */
+    Special<float>::ShowCount();
+    Special<float> *S = new Special<float>();
+    S->display();
+    
+    system("pause");
+    
     Special<float>::ShowCount();
     return 0;
 }
