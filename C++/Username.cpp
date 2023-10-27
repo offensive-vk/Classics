9ef5bd61d8f600c5ce7ed76a3d72b3b5e1b815aa
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-27 17:16:39 +0530

    ~

--- C++/Username.cpp
+++ C++/Username.cpp
@@ -15,3 +15,18 @@ class BadLengthException : public exception{
             return n;
         }
 };
+bool checkUsername(string username)
+{
+	bool isValid = true;
+	int n = username.length();
+	if(n < 5){
+		throw BadLengthException(n);
+	}
+	for(int i = 0; i < n - 1; i++){
+		if(username[i] == 'w' && username[i+1] == 'w'){
+			isValid = false;
+		}
+	}
+
+	return isValid;
+}
