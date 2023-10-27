7c7a30f85d35cb6ed6532ba6d49c9ff95dc7d5c5
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-27 17:39:39 +0530

    ~~

--- C++/PartialTemp.cpp
+++ C++/PartialTemp.cpp
@@ -14,3 +14,23 @@ private:
     K key;
     V value;
 };
+template <typename T>
+class MyPair<T, int> {
+public:
+    MyPair(T k, int v) : key(k), value(v) {}
+
+    void print() const {
+        std::cout << "Partial specialization for int values: key = " << key << ", value = " << value << std::endl;
+    }
+
+private:
+    T key;
+    int value;
+};
+
+int main() {
+    MyPair<double, std::string> p1(3.2, "example");
+    MyPair<char, int> p2('A', 65);
+    p1.print(); // General template: key = 3.2, value = example
+    p2.print(); // Partial specialization for int values: key = A, value = 65
+}
