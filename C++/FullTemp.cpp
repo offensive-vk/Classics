6e1a30442232b4f1f6ca15c342f924771d5df559
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-27 17:37:30 +0530

    ~~

---
+++ C++/FullTemp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+template <typename T>
+void printData(const T& data) {
+    std::cout << "General template: " << data << std::endl;
+}
+
+template <>
+void printData(const char* const & data) {
+    std::cout << "Specialized template for const char*: " << data << std::endl;
+}
+
+int main() {
+    int a = 5;
+    const char* str = "Hello, world!";
+    printData(a);
+    printData(str);
+}
