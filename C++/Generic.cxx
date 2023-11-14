2916303705af23cb77768d79ae58121d518fd901
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-14 17:49:48 +0530

    c++

--- C++/Generic.cxx
+++ C++/Generic.cxx
@@ -2,9 +2,19 @@
 #include <conio.h>
 #include <stdio.h>
 #include <algorithm>
+using namespace std;
+static string name = "";
+template <typename Type> inline int check(Type args) {
+
+    cout << "Enter The Value : " << endl;
+    cin >> name;
+
+    cout << "Your Name is : " << name << endl;
+    return 1;
+}
 
 int main(int argc, char const *argv[])
 {
-    
+    check<int>(false);
     return 0;
 }
