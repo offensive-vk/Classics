297b73bc534443c1faffc7704a4bfa61c5fba94b
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-18 11:06:04 +0530

    C++

--- C++/MagicNumber.cpp
+++ C++/MagicNumber.cpp
@@ -20,13 +20,15 @@ int magicTrick(int originalNumber) {
 
 int main() {
     int originalNumber = 234;
+    std::cout << "Choose your three digit number : " << std::endl;
+    std::cin >> originalNumber;
     int result = magicTrick(originalNumber);
 
     std::cout << "Original Number: " << originalNumber << std::endl;
     std::cout << "Reversed Number: " << reverseNumber(originalNumber) << std::endl;
     std::cout << "Difference: " << abs(originalNumber - reverseNumber(originalNumber)) << std::endl;
     std::cout << "Reversed Difference: " << reverseNumber(abs(originalNumber - reverseNumber(originalNumber))) << std::endl;
-    std::cout << "Sum: " << result << std::endl;
+    std::cout << "Magic Number : " << result << std::endl;
 
     return 0;
 }
