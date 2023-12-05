ba180ba604bf3782ef5b9be924a4f89573c1c2de
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-05 17:00:14 +0530

    cxx

---
+++ C++/Permutations.cxx
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+
+int main(int argc, char **argv[]) {
+    int size = 0;
+    std::cout << "Please Enter The Size of Array : " << std::endl;
+    std::cin >> size;
+    int arr[size];
+
+    for(int i = 0; i < size; i++) {}
+
+}
