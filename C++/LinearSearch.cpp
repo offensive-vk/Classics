88854e571aa3a79942e262c70e3520291f8cb779
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-07 21:40:05 +0530

    c++

--- C++/LinearSearch.cpp
+++ C++/LinearSearch.cpp
@@ -4,15 +4,19 @@ int linearSearch(int arr[], int element) {
     for(int c = 0; c < 5; c++) {
         if(arr[c] == element) {
             std::cout << "Element Found At Index " << c << std::endl;
-            return arr[c];
+            return c;
         }
-        return NULL;
     }
     
 }
 int main(int argc, char **argv[]) {
-    int arr[5] = {534,728,111, 960, 555};
+    int arr[] = {534, 728, 111, 960, 555};
+
+    for(int x: arr) {
+        std::cout << x << std::endl;
+    }
+    
     auto result = linearSearch(arr, 960);
-    std::cout << result << std::endl;
+    std::cout << "Element : "<< arr[result] << std::endl;
     return 0;
 }
