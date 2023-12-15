f347eee48614b834998f2d1dc8c89b6060953c0a
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-15 14:52:51 +0530

    C++

--- C++/print.cxx
+++ C++/print.cxx
@@ -5,11 +5,13 @@
 void print() {
     std::cout << std::endl;
 }
-template <typename Type, typename... Args> void print(Type first, Args... rest) {
+template <typename T, typename... A> void print(T first, A... rest) {
     std::cout << first << " ";
+
     print(rest...);
 }
 
 int main(int argc, char** argv[]) {
-
+    print();
+    print(argc, argv);
 }
