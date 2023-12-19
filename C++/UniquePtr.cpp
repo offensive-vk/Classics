bdb7b247037c1d4b3ad0a420f2f6e23c9814e30b
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-19 11:43:31 +0530

    c++

---
+++ C++/UniquePtr.cpp
@@ -0,0 +1,6 @@
+/**
+std::unique_ptr is a smart pointer provided by the C++ Standard Library. It is a template class that is used for managing single objects or arrays.
+
+unique_ptr works on the concept of exclusive ownership - meaning only one unique_ptr is allowed to own an object at a time. This ownership can be transferred or moved, but it cannot be shared or copied.
+
+*/
