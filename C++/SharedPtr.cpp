a4318cc16ee33c640d56e43653550e798bc19e2f
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-19 11:52:09 +0530

    C++

---
+++ C++/SharedPtr.cpp
@@ -0,0 +1,33 @@
+/**
+A shared_ptr is a type of smart pointer in C++ that allows multiple pointers to share ownership of a dynamically allocated object. The object will be automatically deallocated only when the last shared_ptr that points to it is destroyed.
+
+When using a shared_ptr, the reference counter is automatically incremented every time a new pointer is created, and decremented when each pointer goes out of scope. Once the reference counter reaches zero, the system will clean up the memory.
+*/
+
+#include <iostream>
+#include <memory>
+
+class MyClass {
+public:
+    MyClass() { std::cout << "Constructor is called." << std::endl; }
+    ~MyClass() { std::cout << "Destructor is called." << std::endl; }
+};
+
+int main() {
+    // create a shared pointer to manage the MyClass object
+    std::shared_ptr<MyClass> ptr1(new MyClass());
+    
+    {
+        // create another shared pointer and initialize it with the previously created pointer
+        std::shared_ptr<MyClass> ptr2 = ptr1;
+
+        std::cout << "Inside the inner scope." << std::endl;
+        // both pointers share the same object, and the reference counter has been increased to 2
+    }
+
+    std::cout << "Outside the inner scope." << std::endl;
+    // leaving the inner scope will destroy ptr2, and the reference counter is decremented to 1
+    
+    // the main function returns, ptr1 goes out of scope, and the reference counter becomes 0
+    // this causes the MyClass object to be deleted and the destructor is called
+}
