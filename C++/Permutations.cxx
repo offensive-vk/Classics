94ec44446fa6d18a87e0226b606977b8e18d2389
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-26 14:42:27 +0530

    c++

--- C++/Permutations.cxx
+++ C++/Permutations.cxx
@@ -29,3 +29,41 @@ void Permute(int array[], int start, int size, std::vector<std::vector<int>> &re
         swap(array[start], array[i]);
     }
 }
+int main() {
+    int size = 0;
+    std::cout << "Please Enter The Size of Array : " << std::endl;
+    std::cin >> size;
+
+    int arr[size];
+
+    for (int i = 0; i < size; i++) {
+        std::cout << "Enter The Element At Index " << i << " : ";
+        std::cin >> arr[i];
+    }
+
+    std::cout << "\n==================\n" << std::endl;
+    std::cout << "Array Elements After Sorting : \n" << std::endl;
+
+    for (int val : arr) {
+        std::cout << val << std::endl;
+    }
+
+    std::cout << "\n==================\n" << std::endl;
+    std::cout << "Permutations of the Array : \n" << std::endl;
+
+    // Vector to store permutations
+    std::vector<std::vector<int>> result;
+
+    // Generate permutations
+    Permute(arr, 0, size, result);
+
+    // Display permutations
+    for (const auto &permutation : result) {
+        for (int val : permutation) {
+            std::cout << val << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
