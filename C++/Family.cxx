44f8cf068df3cf8e3b1279e8ce17b2aae5e1d240
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-01-06 17:53:12 +0530

    cxx

--- C++/Family.cxx
+++ C++/Family.cxx
@@ -9,11 +9,18 @@ class Human {
     std::string gender;
 
     Human() { }
-    virtual void die() { }
+    virtual void born( ) { }
+    virtual void breath( ) { }
+    virtual void drink( ) { }
+    virtual void eat( ) { }
     virtual void sleep( ) { }
-
+    virtual void die() { }
 };
-template <typename X> class GrandParents {
+template <typename X> class GrandParents: public Human {
     public:
-
+    GrandParents(std::string &name, std::string &gender, int &age) {
+        this->age = age;
+        this->name = name;
+        this->gender = gender;
+    }
 };
