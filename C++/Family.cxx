9c6afbaf09b7b6715a21148912e661dd00032795
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-01-06 17:48:59 +0530

    cxx

--- C++/Family.cxx
+++ C++/Family.cxx
@@ -2,3 +2,18 @@
 #include <conio.h>
 #include <algorithm>
 
+class Human {
+    public:
+    std::string name;
+    int age;
+    std::string gender;
+
+    Human() { }
+    virtual void die() { }
+    virtual void sleep( ) { }
+
+};
+template <typename X> class GrandParents {
+    public:
+
+};
