7f5072674c1e08e9ffcec6e09392d24ba8922b98
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-01-10 12:09:55 +0530

    C

---
+++ C/main.c
@@ -0,0 +1,13 @@
+extern const int x = 1999;
+extern const long int regex = 580347583745; 
+const char lang[] = "C Old Style";
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+void main() {
+    printf("Program Has Been Executed - %d\n", x);
+    
+}
+
