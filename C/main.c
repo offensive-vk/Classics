58791ba98984064d4f5b7a979aa6b2e647812075
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-01-14 12:57:45 +0530

    main.c

--- C/main.c
+++ C/main.c
@@ -1,5 +1,3 @@
-extern const int x = 1999;
-extern const long int regex = 580347583745; 
 const char lang[] = "C Old Style";
 
 #include <stdio.h>
@@ -7,14 +5,7 @@ const char lang[] = "C Old Style";
 #include <conio.h>
 
 int main(void) {
-    printf("Program Has Been Executed - %d\n", x);
-    int *ptr = &regex;
-    int *global[INT_MAX];
-    for(size_t i = 0; i < INT_MAX; ++i) {
-        global[i] = 794612677L;
-        printf(global[i]);
-    }
+    printf("Program Has Been Executed -");
 
     sprintf("Program Terminated", "");
-
 }
