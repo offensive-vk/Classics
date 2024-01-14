6bab04fa0dde59ec0d258cb0c78bfb7a2740f89c
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-01-14 12:59:15 +0530

    c

--- C/typedef.c
+++ C/typedef.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+// Use case 1: Creating an alias for a basic data type
+typedef int integer;
+
+// Use case 2: Creating an alias for a structure
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+// Use case 3: Creating an alias for a function pointer type
+typedef void (*FunctionPointer)(int);
+
+// Use case 4: Creating an alias for an array type
+typedef int IntArray[5];
