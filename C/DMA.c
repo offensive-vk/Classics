813ced5ec4069fd451efbbc82d55a45ec7e45adc
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-01-19 17:43:49 +0530

    DMA

--- C/DMA.c
+++ C/DMA.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main() {
+    // Using malloc: Allocate memory for an integer
+    int *dynamicInt = (int *)malloc(sizeof(int));
+    if (dynamicInt == NULL) {
+        printf("Memory allocation failed for dynamicInt.\n");
+        return 1; // Exit with an error code
+    }
+
+
+
+    return 0; // Exit successfully
+}
