802b8a118ff60238d827776fa3a864ad8d1e2642
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-01-20 14:29:12 +0530

    Update time.c

--- C/time.c
+++ C/time.c
@@ -1,22 +1,23 @@
 #include<stdio.h>
 #include<math.h>
+//program to take user input and convert seconds into minutes and hours.
 int main()
 {
-	//program 16 to take user input and convert seconds into minutes and hours.
-	printf(">> A Program By Vedansh <<\n");
+	
 	int seconds, sec , minutes, hours;
 	printf(">> Enter A Time in Seconds: ");
 	scanf("%d", &seconds);
 	printf("---------------------\n");
+
 	// seconds into hours.
 	hours = seconds/3600;
 	// seconds into minutes.
 	minutes = (seconds - hours*3600)/60;
 	sec = seconds - hours*3600 - minutes*60;
-	// displaying the outputs.
+
 	printf("\n>> Minutes = %d", minutes);
 	printf("\n>> Hours = %d", hours);
 	printf("\n>> Seconds = %d", sec);
 	
-	return 00;
+	return 0;
 }
