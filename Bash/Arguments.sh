42fb75c76b2ecc9e7d18a38af21a4f89d682fdda
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-02-12 16:06:41 +0530

    ~~

---
+++ Bash/Arguments.sh
@@ -0,0 +1,10 @@
+$1 … $9	Parameter 1 ... 9
+$0	Name of the script itself
+$1	First argument
+${10}	Positional parameter 10
+$#	Number of arguments
+$$	Process id of the shell
+$*	All arguments
+$@	All arguments, starting from first
+$-	Current options
+$_	Last argument of the previous command
