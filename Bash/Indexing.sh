327f589e8905c46730a5a5043d42490c42c5755f
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-02-12 16:09:35 +0530

    ~~~

---
+++ Bash/Indexing.sh
@@ -0,0 +1,9 @@
+${Fruits[0]}	First element
+${Fruits[-1]}	Last element
+${Fruits[*]}	All elements
+${Fruits[@]}	All elements
+${#Fruits[@]}	Number of all
+${#Fruits}	    Length of 1st
+${#Fruits[3]}	Length of nth
+${Fruits[@]:3:2}	Range
+${!Fruits[@]}	Keys of all
