915c917b5e89e57c3e8c3148a68ca214c6f68582
Manish ✨ <143704410+eclipse-manish@users.noreply.github.com>
2024-06-19 19:02:28 +0530

    Minors.
    Co-authored-by: Vedansh ✨ <superuser.ntsystems@outlook.com>

---
+++ Bash/ListAndOutput.sh
@@ -0,0 +1,4 @@
+echo "This example shows how you can list your current working directory and save the output accordingly."
+echo "################################"
+
+echo "################################"
