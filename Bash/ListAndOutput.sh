983c1d0665fdbe29548469aeeae186e428e3ed8c
Manish ✨ <143704410+eclipse-manish@users.noreply.github.com>
2024-06-19 19:04:41 +0530

    Two More....
    Co-authored-by: Vedansh ✨ <superuser.ntsystems@outlook.com>

--- Bash/ListAndOutput.sh
+++ Bash/ListAndOutput.sh
@@ -1,4 +1,4 @@
 echo "This example shows how you can list your current working directory and save the output accordingly."
 echo "################################"
-
+pwd ; ls -carl > ls-results.txt
 echo "################################"
