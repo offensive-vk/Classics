fbb11b394e544b5943382d98658a92b48fb654f0
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-07-07 17:37:27 +0530

    Sampels

--- Algorithms/Testing.cpp
+++ Algorithms/Testing.cpp
@@ -8,7 +8,7 @@ template <class T> class Test {
     T y;
     Test() {/**@param NULL */ }
     ~Test() { /**@default destructor. */}
-    Test(int& user, std::string& name) {
+    Test(int user, std::string& name) {
         std::cout << "Params Done\n" << std::endl;
     }
 };
@@ -16,10 +16,9 @@ template <class T> class Test {
 int main(void) {
     std::cout << "-- Starting Tests --\n ";
     Test<int> T();
-    int *US = 8;
-    std::string *user = "Default User"; 
+    int US = 8;
+    std::string user = "Default User"; 
     
-    Test<int> sample(US, user);
-
+    
     return 0;
 }
