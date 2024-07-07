bc05f38116b6d77e9fe6ee69f070ce5c3ddac0d5
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-07-07 17:20:11 +0530

    Testing...

--- Algorithms/Testing.cpp
+++ Algorithms/Testing.cpp
@@ -1,13 +1,25 @@
 #include<iostream>
 #include<cassert>
-#define FILE "Testing.cpp/*.**"
+#define FILE "Testing.cpp/*.c**"
 
 template <class T> class Test {
     std::string name;
     T x;
-    Test() {/**@param NULL */}
+    T y;
+    Test() {/**@param NULL */ }
     ~Test() { /**@default destructor. */}
-    Test(T& user, std::string& name) {
+    Test(int& user, std::string& name) {
         std::cout << "Params Done\n" << std::endl;
     }
 };
+
+int main(void) {
+    std::cout << "-- Starting Tests --\n ";
+    Test<int> T();
+    int *US = 8;
+    std::string *user = "Default User"; 
+    
+    Test<int> sample(US, user);
+
+    return 0;
+}
