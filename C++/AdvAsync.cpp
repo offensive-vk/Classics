72b65e218bfc43e04ce469702641569869d3dd2e
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-07-21 15:42:46 +0530

    Phases

---
+++ C++/AdvAsync.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <future>
+#include <vector>
+#include <chrono>
+#include <algorithm>
+#include <bits/c++io.h>
+
+void getInput() {
+    std::string name;
+    std::string gender;
+    unsigned int age;
+
+    std::cout << "Enter Your Name: " << std::endl;
+    std::cin >> name;
+    std::cout << "Enter Your Gender: " << std::endl;
+    std::cin >> gender;
+    std::cout << "Enter Your Age: " << std::endl;
+    std::cin >> age;
+
+}
